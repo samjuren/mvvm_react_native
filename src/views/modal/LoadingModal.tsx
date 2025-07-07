79aa4ac24a785c29396a0eb6bc82120759e2bc22
Samuel Juren <samuel.costa@onet-brasil.com.br>
2025-07-07 17:59:43 -0300

    ajuste code

--- src/views/modal/LoadingModal.tsx
+++ src/views/modal/LoadingModal.tsx
@@ -1,6 +1,7 @@
 // src/components/LoadingModal.tsx
 import React from 'react';
 import { Modal, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { styles } from './styles'
 
 interface Props {
   visible: boolean;
@@ -10,17 +11,10 @@ export default function LoadingModal({ visible }: Props) {
   return (
     <Modal transparent visible={visible} animationType="fade">
       <View style={styles.overlay}>
-        <ActivityIndicator size="large" color="#E15610" />
+          <ActivityIndicator 
+              size="large" 
+              color="#E15610" />
       </View>
     </Modal>
   );
 }
-
-const styles = StyleSheet.create({
-  overlay: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
---
+++ src/views/modal/styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
